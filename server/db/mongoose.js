b4d7954ffc9f310be0fcb5c904b387bfdece2faf
pawel <pawel.html@gmail.com>
2017-07-12 19:26:16 +0200

    Attempt do connect to DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<pawellesniowski>:<Matrix0019M>@ds155582.mlab.com:55582/mlab2');
+mongoose.connect('mongodb://<pawel>:<matrix>@ds155582.mlab.com:55582/mlab2');
 
 module.exports = {mongoose};
