d7291476365de1c0b8a67a5de11da8743badab9f
pawel <pawel.html@gmail.com>
2017-07-12 19:21:34 +0200

    make me better

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<pawellesniowski>:<Matrix0019M>@ds155582.mlab.com:55582/mlab2');
 
 module.exports = {mongoose};
